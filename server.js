8b5d2af312992318025a592c1046ab95ef5971e3
tanu234234 <tanushree9663@gmail.com>
2026-03-24 18:14:11 +0530

    fixed deployment port

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 app.use(express.static("public"));
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
-});
 
 io.on("connection", (socket) => {
     console.log("User connected");
